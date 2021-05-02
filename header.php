a6c31c5acc36696f18ea9f5936211b5503ec7a9e
Sahil Hamirani <sahil.hamirani@mindinventory.com>
2021-05-02 13:16:25 +0530

    Manage space in header

--- header.php
+++ header.php
@@ -1,34 +1,34 @@
 <?php
-	ob_end_flush();
-	ob_start();
-	
-	session_start();
-	
-	include_once("names.php");
-	
+ob_end_flush();
+ob_start();
+session_start();
+
+include_once("names.php");
+
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="<?php echo $url; ?>/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $url; ?>/bootstrap/w3.css">
     <link href="<?php echo $url; ?>/style.css" rel="stylesheet">
-	
-	<script src="<?php echo $url; ?>/bootstrap/jquery.min.js"></script>
-	<script src="<?php echo $url; ?>/bootstrap/bootstrap.min.js"></script>
-	<meta name="propeller" content="f1e6bf35efc28edac2049438a11d40ed">
+
+    <script src="<?php echo $url; ?>/bootstrap/jquery.min.js"></script>
+    <script src="<?php echo $url; ?>/bootstrap/bootstrap.min.js"></script>
+    <meta name="propeller" content="f1e6bf35efc28edac2049438a11d40ed">
 
 
-<script data-ad-client="ca-pub-5861328993701577" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-     <!-- <script data-ad-client="ca-pub-5861328993701577" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
+    <script data-ad-client="ca-pub-5861328993701577" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- <script data-ad-client="ca-pub-5861328993701577" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
 
-<!--     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!--     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
     google_ad_client: "ca-pub-6376552725660392",
@@ -37,7 +37,7 @@
 </script>
  -->
 
-<!-- <script data-ad-client="ca-pub-4610357151191256" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- <script data-ad-client="ca-pub-4610357151191256" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
@@ -50,88 +50,85 @@
 
 <body>
 
-	<?php include_once("analyticstracking.php"); ?>
-	
+    <?php include_once("analyticstracking.php"); ?>
+
     <?php
-	if(isset($_SESSION[$s_name]))
-	{
-		$username = $_SESSION[$s_name];
-	?>
+    if (isset($_SESSION[$s_name])) {
+        $username = $_SESSION[$s_name];
+    ?>
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
                     </button>
-                        <a href="<?php echo $url; ?>" class="navbar-brand"> <span class="glyphicon glyphicon-home"></span>  &nbps; <?php echo $title; ?>  </a>
-                </div>   
-                <div>  
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li> <a href="<?=$url?>/account/check"><span class="glyphicon glyphicon-user"></span> <?php echo $username; ?> </a> </li>
-                        <li> <a href="<?=$url?>/addsms/new"><span class="glyphicon glyphicon-file"></span> Add SMS </a> </li>
-                        <li> <a href="<?=$url?>/mysms/all/1"><span class="glyphicon glyphicon-file"></span> My SMS </a> </li>
-                        <li> <a href="<?=$url?>/logout"><span class="glyphicon glyphicon-off"></span> Logout </a> </li>
-						<li> <a href="<?=$url?>/disclaimer"><span class="glyphicon glyphicon-book"></span> Disclaimer </a> </li>
-						<li> <a href="mailto:dailysmsmaza@gmail.com"><span class="glyphicon glyphicon-book"></span> Contact Us </a> </li>
-                    </ul>
-                 </div>
+                    <a href="<?php echo $url; ?>" class="navbar-brand"> <span class="glyphicon glyphicon-home"></span> &nbps; <?php echo $title; ?> </a>
                 </div>
-         </div>      
-        </div>
+                <div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li> <a href="<?= $url ?>/account/check"><span class="glyphicon glyphicon-user"></span> <?php echo $username; ?> </a> </li>
+                            <li> <a href="<?= $url ?>/addsms/new"><span class="glyphicon glyphicon-file"></span> Add SMS </a> </li>
+                            <li> <a href="<?= $url ?>/mysms/all/1"><span class="glyphicon glyphicon-file"></span> My SMS </a> </li>
+                            <li> <a href="<?= $url ?>/logout"><span class="glyphicon glyphicon-off"></span> Logout </a> </li>
+                            <li> <a href="<?= $url ?>/disclaimer"><span class="glyphicon glyphicon-book"></span> Disclaimer </a> </li>
+                            <li> <a href="mailto:dailysmsmaza@gmail.com"><span class="glyphicon glyphicon-book"></span> Contact Us </a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
         </nav>
-		
-		
+
+
     <?php
-	}
-	else
-	{
-	?>
-    	<nav class="navbar navbar-default">
+    } else {
+    ?>
+        <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
                     </button>
-                        <a href="<?php echo $url; ?>" class="navbar-brand"><span class="glyphicon glyphicon-home"> </span> &nbsp; <?php echo $title; ?> </a>
-                </div>   
-                <div>  
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li> <a href="<?=$url?>/login/getinfo"><span class="glyphicon glyphicon-log-in"></span> Log In </a> </li>
-                        <li> <a href="<?=$url?>/register/getinfo"> <span class="glyphicon glyphicon-user"></span> Register </a> </li>
-						<li> <a href="<?=$url?>/disclaimerisc"><span class="glyphicon glyphicon-book"></span> Disclaimer </a> </li>
-						<li> <a href="mailto:dailysmsmaza@gmail.com"><span class="glyphicon glyphicon-book"></span> Contact Us </a> </li>
-                    </ul>
-                 </div>
+                    <a href="<?php echo $url; ?>" class="navbar-brand"><span class="glyphicon glyphicon-home"> </span> &nbsp; <?php echo $title; ?> </a>
                 </div>
-         </div>      
-        </div>
+                <div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li> <a href="<?= $url ?>/login/getinfo"><span class="glyphicon glyphicon-log-in"></span> Log In </a> </li>
+                            <li> <a href="<?= $url ?>/register/getinfo"> <span class="glyphicon glyphicon-user"></span> Register </a> </li>
+                            <li> <a href="<?= $url ?>/disclaimerisc"><span class="glyphicon glyphicon-book"></span> Disclaimer </a> </li>
+                            <li> <a href="mailto:dailysmsmaza@gmail.com"><span class="glyphicon glyphicon-book"></span> Contact Us </a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
         </nav>
-        
-    <?php	
-	}
-	?>
-	
 
-	<div class="container">
-		<form class="navbar-form navbar-right" method="POST">
-		   <div class="input-group">
-			   <input type="text" name="search" placeholder="Search..." class="form-control" />
-			   <div class="input-group-btn">
-				   <button class="btn btn-info">
-				   <span class="glyphicon glyphicon-search"></span>
-				   </button>
-			   </div>
-		   </div>
-		</form>
-	</div>
+    <?php
+    }
+    ?>
 
-	<!--
+
+    <div class="container">
+        <form class="navbar-form navbar-right" method="POST">
+            <div class="input-group">
+                <input type="text" name="search" placeholder="Search..." class="form-control" />
+                <div class="input-group-btn">
+                    <button class="btn btn-info">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!--
 	<div class="visible-sm visible-xs">
 		<center> 
 		 <a href="https://play.google.com/store/apps/details?id=com.happytechsolution.mathematicspuzzle" target="_blank">
@@ -141,23 +138,23 @@
 	</div>
 	-->
     <div class="container visible-lg">
-		<nav class="navbar navbar-default">
+        <nav class="navbar navbar-default">
             <ul class="nav navbar-nav">
                 <li> <a href="<?php echo $url; ?>/last/updated/sms/new2old/page/1" class="list-group-item"> Last / Latest Updated Sms </a> </li>
                 <!-- <li> <a href="<?php echo $url; ?>/popular/most/sms/new2old/page/1" class="list-group-item"> Popular Sms </a> </li> -->
                 <!-- <li> <a href="<?php echo $url; ?>/top/user/page/1" class="list-group-item"> Top User </a> </li> -->
-               <!-- <li> <a href="#" class="list-group-item"> Add Your Sms </a> </li> -->
-             </ul>             
-    	</nav>
+                <!-- <li> <a href="#" class="list-group-item"> Add Your Sms </a> </li> -->
+            </ul>
+        </nav>
     </div>
-	
-	<?php
-		if(isset($_POST["search"]))
-		{
-			$search = $_POST["search"];
-			header("location:$url/search/$search/page/1");
-		}
-	?>
-    
+
+    <?php
+    if (isset($_POST["search"])) {
+        $search = $_POST["search"];
+        header("location:$url/search/$search/page/1");
+    }
+    ?>
+
 </body>
+
 </html>
