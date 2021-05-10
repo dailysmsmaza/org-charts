07ab884e7c3fac645d964e84cbb6cca2f189af96
Sahil Hamirani <sahil.hamirani@mindinventory.com>
2021-05-11 02:01:20 +0530

    Manage connection error

--- connect.php
+++ connect.php
@@ -3,10 +3,10 @@
 
 if (CURRENT_MODE == PRODUCTION) {
 	//LIVE DBF
-	$c = mysqli_connect('localhost', 'hurvotmy_dsmadmin', 'xB@U&1g&T&AU', 'hurvotmy_dailysmsmaza') or die("Connection Error");
+	$c = mysqli_connect('localhost', 'hurvotmy_dsmadmin', 'xB@U&1g&T&AU', 'hurvotmy_dailysmsmaza') or die("Live DB Connection Error");
 } else {
 	//LOCAL DB
-	$c = mysqli_connect('localhost','root','','dailysmsmaza') or die("Connection Error");
+	$c = mysqli_connect('localhost','root','','dailysmsmaza') or die("Local DB Connection Error");
 
 }
 mysqli_query($c, "SET NAMES 'utf8mb4'"); 
