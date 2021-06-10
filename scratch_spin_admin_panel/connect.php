244facfbce66604b383780f0eef95d1b252424de
dailysmsmaza.com <dailysmsmaza.com@gmail.com>
2021-06-10 13:59:11 +0530

    connect file changes

--- scratch_spin_admin_panel/connect.php
+++ scratch_spin_admin_panel/connect.php
@@ -3,7 +3,7 @@ error_reporting(0);
 $host='localhost';   //mainly localhost accepted in major cases but if needed change it with your host
 $user='hurvotmy_dsmadmin';  // change it with your database user
 $password='xB@U&1g&T&AU';  // change it with your database password
-$db='mywallpa_tested';   // change it with your database name
+$db='hurvotmy_scratch_and_spin';   // change it with your database name
 
 $conn=mysqli_connect($host,$user,$password,$db);
 
